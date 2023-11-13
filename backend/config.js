5c5720b00787a31a8bf04501297909bba11b1e70
Dev <devbajaj.bits@gmail.com>
2023-11-14 00:18:06 +0530

    Remove backend/config.js from version control

--- backend/config.js
+++
@@ -1,4 +0,0 @@
-export const PORT = 5555;
-
-export const mongoDBURL =
-"mongodb+srv://devbajajbits:lv9kDWCssm0JlbKO@book-store-mern.ryk3v92.mongodb.net/books-collection?retryWrites=true&w=majority"
